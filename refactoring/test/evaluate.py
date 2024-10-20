ce48f1d1a228a7c5b924f9838e3f35803676f1f4
C4RLO5lg <117377625+C4RLO5lg@users.noreply.github.com>
2024-10-20 12:39:21 -0600

    Added model evaluation script

---
+++ refactoring/test/evaluate.py
@@ -0,0 +1,103 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import mlflow
+import os
+import argparse
+import json
+import joblib
+
+class ModelEvaluator:
+    def __init__(self, X_test_path, y_test_path, model_path):
+        self.X_test_path = X_test_path
+        self.y_test_path = y_test_path
+        self.param = model_path
+        self.experiment_name = self.params['mlflow']['experiment_name'] + self.version_suffix
+
+    def load_data(self):
+        try:
+            X_test = pd.read_csv(self.X_test_path)
+            y_test = pd.read_csv(self.y_test_path).values.ravel()
+            return X_test, y_test
+        except FileNotFoundError as e:
+            print(f"Error loading test data: {e}")
+            raise
+
+    def load_model(self):
+        try:
+            model = joblib.load(self.model_path)
+            return model
+        except FileNotFoundError as e:
+            print(f"Error loading model: {e}")
+            raise
+        except joblib.JoblibException as e:
+            print(f"Error in joblib: {e}")
+            raise
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            raise
+
+    def evaluate_model(self, model, X_test, y_test):
+        y_pred = model.predict(X_test)
+
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+
+        return accuracy, precision, recall, y_pred
+
+    def log_metrics(self, accuracy, precision, recall):
+        mlflow.log_metric("test_accuracy", accuracy)
+        mlflow.log_metric("test_precision", precision)
+        mlflow.log_metric("test_recall", recall)
+
+    def log_predictions(self, y_test, y_pred):
+        pred_df = pd.DataFrame({"actual": y_test.values.ravel(), "predicted": y_pred})
+        pred_df.to_csv("data/predictions/predictions.csv", index=False)
+        mlflow.log_artifact("data/predictions/predictions.csv")
+
+    def save_metrics(self, metrics, output_path):
+        with open(output_path, 'w') as f:
+            json.dump(metrics, f)
+
+    def ensure_experiment_active(self):
+        client = mlflow.tracking.MlflowClient()
+        experiment = client.get_experiment_by_name(self.experiment_name)
+        if experiment:
+            if experiment.lifecycle_stage == "deleted":
+                client.restore_experiment(experiment.experiment_id)
+                print(f"Experiment '{self.experiment_name}' restored.")
+            return experiment.experiment_id
+        else:
+            experiment_id = mlflow.create_experiment(self.experiment_name)
+            print(f"Experiment '{self.experiment_name}' created.")
+            return experiment_id
+
+    def run_evaluation(self):
+        experiment_id = self.ensure_experiment_active()
+        mlflow.set_experiment(experiment_id=experiment_id)
+        with mlflow.start_run():
+            X_test, y_test = self.load_data()
+            model = self.load_model()
+
+            accuracy, precison, recall, y_pred = self.evaluate_model(model, X_test, y_test)
+            self.log_metrics(accuracy, precison, recall)
+
+            self.log_predictions(y_test, y_pred)
+
+            metrics = {
+                "accuracy": accuracy,
+                "precison": precison,
+                "recall": recall
+            }
+            os.makedirs("metrics", exist_ok=True)
+            self.save_metrics(metrics, "metrics/evaluation.json")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate model")
+    parser.add_argument("--X_test_path", type=str, help="Path to X_test.csv")
+    parser.add_argument("--y_test_path", type=str, help="Path to y_test.csv")
+    parser.add_argument("--model_path", type=str, help="Path to the best model file")
+    args = parser.parse_args()
+
+    me = ModelEvaluator(args.X_test_path, args.y_test_path, args.model_path)
+    me.run_evaluation()
